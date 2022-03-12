7a08e52b42084b17885505a99f504df86d7f308d
张涛 <tao.zhang@i-tudou.com>
2022-03-12 17:38:08 +0800

    新建mongodb工具模块二次封装

--- tools/api_tool_request.py
+++ tools/api_tool_request.py
@@ -167,15 +167,44 @@ class Requests:
             return f"发送请求异常:{e}"
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     # url = "http://192.168.0.115:10007/kuberegistry/v1/api/repository"
     #
-    # headers = {
-    #     'Content-Type': 'application/json;charset=UTF-8',
-    #     'Authorization': 'Bearer a5244cdb-d41e-4046-bf62-bd4ca5511d8c'
-    # }
+    headers = {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Authorization': 'Bearer 6317f32b-c5a7-4a82-8958-456ce06857eb'
+    }
     #
     # data = {"tagSet": ["分析工具"], "imageName": "zhangtao_test_001", "groupId": 495, "tenantId": 1, "description": "张涛测试"}
     #
     # res = Requests().send_post(url=url, data=data, headers=headers)
     # print(res.status_code)
+
+    url = 'http://10.0.34.13:10007/kube/v1/api/argo/task'
+    data = {
+        "name": "张涛创建三方测试05",
+        "flowId": "5b0786e4751848c7a4c8be859085b9d4",
+        "originalId": "oj.task.i.287036935841771520",
+        "originalName": "三维3",
+        "description": "张涛创建三方测试05",
+        "queueName": "test_queue",
+        "priority": "100",
+        "paramList": [
+            {
+                "name": "msg",
+                "value": "hello",
+                "description": "打印信息"
+            },
+            {
+                "name": "time",
+                "value": "10",
+                "description": "时间"
+            }
+        ],
+        "extensions": {
+            "app_code": "KF00001",
+            "app_code_biz": "KF00002"
+        }
+    }
+    res = Requests().send_post(url=url, data=data, headers=headers)
+    print(res.json())
