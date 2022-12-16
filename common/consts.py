794280d2733daae9def02372a87cf5fa91f3f9b4
ZhangTao <948080782@qq.com>
2022-12-16 10:20:08 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch docker_jenkins_wanmeiban
    Your branch is up to date with 'origin/docker_jenkins_wanmeiban'.
    
    Changes to be committed:
    	modified:   common/consts.py
    	modified:   main.py

--- common/consts.py
+++ common/consts.py
@@ -8,13 +8,15 @@ from common.readConfigYaml import Config
 TESTCASEPATH = setting.API_EXCEL_FILE
 
 # 获取Jenkins选项参数(切记参数名称不可以使用中文)
-# /
+ENVIRONMENT: str = os.environ['ENVIRONMENT']
+API_HOST: str = os.environ['APIHOST']
+TESTER: str = os.environ['TESTER']
 
 C = Config()
 # 登录人名称
-TESTER: str = C.get_testers()[0]
-API_HOST: str = C.get_api_host()[0]
-ENVIRONMENT: str = C.get_environment_type()[0]
+# ENVIRONMENT: str = C.get_environment_type()[0]
+# API_HOST: str = C.get_api_host()[0]
+# TESTER: str = C.get_testers()[0]
 
 
 LOGIN_URL: str = C.get_login_url(ENVIRONMENT)
